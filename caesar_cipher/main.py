eff17afc7a2c2694f6ef4f979ff62767de9fdb04
Samuel Porto <samuelpporto@gmail.com>
2023-03-07 16:49:07 -0300

    Created decrypt function

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -14,10 +14,25 @@ def encrypt(plain_text, shift_amount):
         position = alphabet.index(letter)
         new_position = position + shift_amount
         if new_position > len(alphabet):
-            new_position = (new_position - len(alphabet))
+            new_position = new_position - len(alphabet)
         encrypted_text += alphabet[new_position]
         
     print(f"Encrypted text: {encrypted_text}")
 
-#Call the encrypt function
-encrypt(text, shift)
+#Function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(plain_text, shift_amount):
+    decrypted_text = ""
+
+    #Shift each letter of the 'text' backwards in the alphabet by the shift amount and print the decrypted text. 
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        decrypted_text += alphabet[new_position]
+    
+    print(f"Decrypted text: {decrypted_text}")
+
+#Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function
+if direction == "encode":
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
