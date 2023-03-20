f45903b5b8949b9474a9406162ea1170c274cb56
Samuel Porto <samuelpporto@gmail.com>
2023-03-20 16:15:17 -0300

    Created a snake controlled with keyboard using turtle

---
+++ snake_game/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            snake = Turtle("square")
+            snake.color("white")
+            snake.penup() 
+            snake.setposition(position)
+            self.segments.append(snake)
+
+    def move(self):
+        for seg in range(len(self.segments) - 1 , 0 , -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].setposition(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segments[0].heading() != DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.segments[0].heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def right(self):
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
+
+    def left(self):
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
