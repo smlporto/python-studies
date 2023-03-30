175fc3c5351b37cfe203ac1709c02e925571fb94
Samuel Porto <samuelpporto@gmail.com>
2023-03-30 13:45:19 -0300

    Added exception handling to the nato phonetic alphabet

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -3,9 +3,16 @@ import pandas
 nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
-print(nato_dict)
 
-word = input("Enter a word: ").upper()
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
 
-letters_list = [nato_dict[letter] for letter in word]
-print(letters_list)
+    try:
+        letters_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters allowed.")
+        generate_phonetic()
+    else:
+        print(letters_list)
+
+generate_phonetic()
