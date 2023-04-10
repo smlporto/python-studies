f5265760f5a4eb81daa9b95de832d602a91ecc53
Samuel Porto <43504710+smlporto@users.noreply.github.com>
2023-04-10 15:55:39 -0300

    Tried delete and update methods

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -22,7 +22,11 @@ graph_params = {
 
 add_pixel_params = {
     "date": datetime.now().strftime("%Y%m%d"),
-    "quantity": "5",
+    "quantity": input("How many hours did you study Pixelas today? "),
+}
+
+new_pixel_params = {
+    "quantity": "2"
 }
 
 headers = {
@@ -32,6 +36,7 @@ headers = {
 user_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{user_endpoint}/{USERNAME}/graphs"
 add_pixel_endpoint = f"{user_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+update_delete_pixel_endpoint = f"{user_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{datetime.now().strftime('%Y%m%d')}"
 
 #response = requests.post(url=user_endpoint, json=user_params)
 #print(response.text)
@@ -41,3 +46,9 @@ add_pixel_endpoint = f"{user_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
 response = requests.post(url=add_pixel_endpoint, json=add_pixel_params, headers=headers)
 print(response.text)
+
+#response = requests.put(url=update_pixel_endpoint, json=new_pixel_params, headers=headers)
+#print(response.text)
+
+#response = requests.delete(url=update_delete_pixel_endpoint, headers=headers)
+#print(response.text)
