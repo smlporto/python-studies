bb2f8e00a8898e393012008a5415cd094ac0e992
Samuel Porto <43504710+smlporto@users.noreply.github.com>
2023-04-10 15:25:37 -0300

    Added a graph to the habit-tracker

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -11,8 +11,24 @@ user_params ={
     "notMinor": "yes"
 }
 
-user_endpoint = "https://pixe.la/v1/users"
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "LearningPixela",
+    "unit": "Hours",
+    "type": "float",
+    "color": "ichou"
+}
 
-response = requests.post(url=user_endpoint, json=user_params)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+user_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{user_endpoint}/{USERNAME}/graphs"
+
+#response = requests.post(url=user_endpoint, json=user_params)
+#print(response.text)
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
 print(response.text)
 
