26512b73f0d227efe0ca1fabae73e84a347a8454
Samuel Porto <43504710+smlporto@users.noreply.github.com>
2023-04-13 16:18:40 -0300

    Added city code match with Tequila API

--- flight-deals-tracker/flight_search.py
+++ flight-deals-tracker/flight_search.py
@@ -1,8 +1,22 @@
+from config import *
+import requests
+
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
 class FlightSearch:
 
     def __init__(self):
         pass
     
     def add_iataCode(self, city_name):
-        code = "TESTING"
+        headers = {
+            "apikey": TEQUILA_API_KEY,
+        }
+        params = {
+            "term": city_name,
+            "location_types": "city"
+        }
+        response = requests.get(url=F"{TEQUILA_ENDPOINT}/locations/query", params=params, headers=headers)
+        city = response.json()["locations"]
+        city_code = city[0]["code"]
+        code = city_code
         return code
