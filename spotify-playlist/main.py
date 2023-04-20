cf02de3ef8be80b008b74e3210e90ad4b5f629d0
Samuel Porto <43504710+smlporto@users.noreply.github.com>
2023-04-20 11:25:23 -0300

    Added authentication to create playlists at spotify

--- spotify-playlist/main.py
+++ spotify-playlist/main.py
@@ -1,10 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
+from config import *
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-URL = "https://www.billboard.com/charts/hot-100/"
+BILLBOARD_URL = "https://www.billboard.com/charts/hot-100/"
+REDIRECT_URI = "https://example.com"
+
+scope = "playlist-modify-private"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID, 
+        client_secret=CLIENT_SECRET, 
+        redirect_uri=REDIRECT_URI, 
+        scope=scope,
+        show_dialog=True,
+        cache_path="token.txt"))
+
+user_id = sp.current_user()["id"]
+
 date = input("Which date do you want to travel to? (YYYY-MM-DD): ")
 
-response = requests.get(f"{URL}" + date)
+response = requests.get(f"{BILLBOARD_URL}" + date)
 web_page = response.text
 
 soup = BeautifulSoup(web_page, "html.parser")
